cdf09f333ef7cb84fab10907867e6cc717b71088
Alex Terinte <terintealexandrin18@gmail.com>
2024-03-03 20:14:30 +0000

    add comment CRUD

--- about/views.py
+++ about/views.py
@@ -5,16 +5,15 @@ from .forms import CollaborateForm
 
 
 def about_me(request):
-    about = About.objects.all().order_by('-updated_on').first()
-    collaborate_form = CollaborateForm()
 
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
-            collaborate_form = CollaborateForm()
-            messages.add_message(request, messages.SUCCESS, 
-            "Collaboration request received! I endeavor to respond within 2 working days.")
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
 
+    about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
